6275de7be61a0fb8c54edc53322eba3fd5819c51
Petr Hubík <petr@hubik.net>
2023-04-26 13:23:15 +0200

    Multiple consumer stop consuming after number of messages received fix

--- src/Kdyby/RabbitMq/MultipleConsumer.php
+++ src/Kdyby/RabbitMq/MultipleConsumer.php
@@ -78,6 +78,14 @@ class MultipleConsumer extends Consumer
 	}
 
 
+	public function stopConsuming()
+	{
+		foreach ($this->queues as $name => $options) {
+			$this->getChannel()->basic_cancel($this->getQueueConsumerTag($name));
+		}
+		$this->onStop($this);
+	}
+
 
 	public function processQueueMessage($queueName, AMQPMessage $msg)
 	{
